2053dca7de35bedba234ce7043e31784e20c385a
Neto test <89373786148@yandex.ru>
2023-02-19 19:49:49 +0300

    second commit

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -1,2 +1,39 @@
 public class PosterManager {
+    private  String[] poster = new String[0];
+    private final int limit;
+
+    public PosterManager() {
+        this.limit = 10;
+    }
+
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addPost(String post) {
+        String[] tmp = new String[poster.length + 1];
+        for (int i = 0; i < poster.length; i++) {
+            tmp[i] = poster[i];
+        }
+        tmp[tmp.length - 1] = post;
+        poster = tmp;
+    }
+
+    public String[] findAll() {
+        return poster;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (poster.length < limit) {
+            resultLength = poster.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = poster[poster.length - 1 - i];
+        }
+        return tmp;
+    }
 }
--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -1,2 +1,82 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PosterManagerTest {
+    String post1 = "Бладшот";
+    String post2 = "Вперед";
+    String post3 = "Отель Белград";
+    String post4 = "Джентельмены";
+    String post5 = "Человек-невидимка";
+    String post6 = "Тролли. Мировой тур";
+    String post7 = "Номер один";
+    String post8 = "Аватар";
+    String post9 = "Отряд самоубийц";
+    String post10 = "Шрек";
+    String post11 = "Терминатор";
+
+    @Test
+    public void findAllTest() {
+
+        PosterManager manager = new PosterManager();
+
+        manager.addPost(post1);
+        manager.addPost(post2);
+        manager.addPost(post3);
+        manager.addPost(post4);
+        manager.addPost(post5);
+        manager.addPost(post6);
+        manager.addPost(post7);
+        manager.addPost(post8);
+        manager.addPost(post9);
+        manager.addPost(post10);
+
+        String[] axpected = {post1, post2, post3, post4, post5, post6, post7, post8, post9, post10};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(axpected, actual);
+    }
+
+    @Test
+    public void findAllLimitTest() {
+
+        PosterManager manager = new PosterManager(11);
+
+        manager.addPost(post1);
+        manager.addPost(post2);
+        manager.addPost(post3);
+        manager.addPost(post4);
+        manager.addPost(post5);
+        manager.addPost(post6);
+        manager.addPost(post7);
+        manager.addPost(post8);
+        manager.addPost(post9);
+        manager.addPost(post10);
+        manager.addPost(post11);
+
+        String[] axpected = {post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(axpected, actual);
+    }
+
+    @Test
+    public void findLastTest() {
+
+        PosterManager manager = new PosterManager();
+        manager.addPost(post1);
+        manager.addPost(post2);
+        manager.addPost(post3);
+        manager.addPost(post4);
+        manager.addPost(post5);
+        manager.addPost(post6);
+        manager.addPost(post7);
+        manager.addPost(post8);
+        manager.addPost(post9);
+        manager.addPost(post10);
+
+        String[] axpected = {post10, post9, post8, post7, post6, post5, post4, post3, post2, post1};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(axpected, actual);
+    }
 }
